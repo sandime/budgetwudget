08bc85e52ebe36422ec0e982a33f044bba1c3f05
sherri <sherri@sherricruz.com>
2015-03-19 19:45:40 -0700

    Initial commit

---
+++ budget.js
@@ -0,0 +1,25 @@
+/**
+ * Created by SHERRI on 3/19/15.
+ */
+function TestCtrl($scope) {
+
+
+    $scope.totalExpenses = function(){
+        var rent = $scope.rent;
+        var utilities = $scope.utilities;
+        var autoPayment = $scope.autoPayment;
+
+
+        return rent + utilities + autoPayment;
+    }
+
+
+    $scope.difference = function(){
+        var income = $scope.income();
+
+
+        return income + totalExpenses;
+
+    }
+
+}
